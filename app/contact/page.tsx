9195f50c72e96a4938d7e9e589016098cf5bbc2d
Joey Lukner <josephlukner@icloud.com>
2025-12-14 23:34:50 -0600

    Started contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
     <div className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <Link href={"/"}>
