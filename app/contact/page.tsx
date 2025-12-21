1191b26db5d25fd18784e31364883b4818822bf0
Joey Lukner <josephlukner@icloud.com>
2025-12-20 23:57:33 -0600

    Added input fields

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,9 @@ import Link from "next/link";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
-  example: string;
-  exampleRequired: string;
+  fullName: string;
+  emailAddress: string;
+  message: string;
 };
 
 export default function Contact() {
@@ -17,7 +18,7 @@ export default function Contact() {
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("fullName")); // watch input value by passing the name of it
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -29,16 +30,30 @@ export default function Contact() {
         </div>
         <Image src="/headshot.JPG" alt="headshot" width={250} height={250} />
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            className="form-standard"
-            defaultValue="test"
-            {...register("example")}
-          />
-          <input
-            className="form-standard"
-            {...register("exampleRequired", { required: true })}
-          />
-          {errors.exampleRequired && <span>This field is required</span>}
+          <label>
+            Full name
+            <input
+              className="form-standard"
+              defaultValue="test"
+              {...register("fullName")}
+            />
+          </label>
+          <label>
+            Email address
+            <input
+              className="form-standard"
+              {...register("emailAddress", { required: true })}
+            />
+          </label>
+          <label>
+            Message{" "}
+            <input
+              className="form-standard"
+              {...register("message", { required: true })}
+            />
+          </label>
+
+          {errors.message && <span>This field is required</span>}
           <input className="form-standard" type="submit" />
         </form>
       </div>
