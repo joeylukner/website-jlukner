ebc9e26f0572910686ad384c2c751c4f23e99e54
Joey Lukner <josephlukner@icloud.com>
2026-01-02 23:59:36 -0600

    Books

--- app/assets/texts.ts
+++ app/assets/texts.ts
@@ -4,7 +4,29 @@ a book per week. Throughout this time, my tastes have changed a lot and certain
 preferences have stayed the same. This is a digital compilation of all of my all-time
 favorites.
 \u2022 Jitterbug perfume, by Tom Robbins
-\u2022 The Secret History by Donna Tartt`;
+\u2022 Still Life With Woodpecker by Tom Robbins
+\u2022 The Secret History by Donna Tartt
+\u2022 Crazy Rich Asians by Kevin Kwan
+\u2022 New Moon by Stephanie Meyer
+\u2022 Carry On by Rainbow Rowell
+\u2022 The Song of Achilles by Madeline Miller
+\u2022 The Book of Salt by Madeline Truong
+\u2022 The Wrath of Dionysus: A Novel by Nagrodskaia Evdokia
+\u2022 One Hundred Years of Solitude by Gabriel García Márquez
+\u2022 Love in the Time of Cholera by Gabriel García Márquez
+\u2022 The Pillars of the Earth by Ken Follett
+\u2022 World Without End by Ken Follett
+\u2022 The Dutch House by Ann Patchett
+\u2022 Pride and Prejudice by Jane Austen
+\u2022 Swann's Way by Marcel Proust
+\u2022 Ladies of the Rachmaninoff Eyes by Henry Van Dyke
+\u2022 Confessions of a Mask by Yukio Mishima
+\u2022 Anna Karenina by Leo Tolstoy
+\u2022 High Maintenance by Jennifer Belle
+\u2022 Jane Eyre by Charlotte Brontë
+\u2022 American Psycho by Bret Easton Ellis
+
+`;
 
 export const welcome: string = `Welcome to my website! I'm a software developer 
 with 2 years of professional experience. I started coding in high school and 
