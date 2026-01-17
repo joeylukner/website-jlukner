e8a1e222ab45240cb9aed87c4c563d23eb9b7c53
Joey Lukner <josephlukner@icloud.com>
2026-01-16 22:38:58 -0500

    Factored out arrays

--- app/assets/texts.ts
+++ app/assets/texts.ts
@@ -1,30 +1,23 @@
-export const libraryText: string = `Welcome to my library. I spent a lot of free time reading.
-Every year since 2020, I've completed a 52-book challenge, reading an average of
-a book per week. Throughout this time, my tastes have changed a lot and certain 
-preferences have stayed the same. This is a digital compilation of all of my all-time
-favorites.
+export const libraryText: string = `Welcome to my library. I spend a lot of free time reading.
+I complete a reading challenge each year. For 2026, my goal is 60 books. Throughout reading journey, 
+my tastes have changed a lot and certain preferences have stayed the same. This is a digital 
+compilation of all of my all-time favorites.
 
 `;
+export const litFictionSubtitle: string = "Explore contemporary literary fiction exploring complex themes and character development.";
 
-export const yaGold: string = `
-\u2022 The Song of Achilles by Madeline Miller
-\u2022 Carry On by Rainbow Rowell
-\u2022 New Moon by Stephanie Meyer
+export const classicsSubtitle: string = "Timeless masterpieces that have shaped literature and culture throughout history.";
 
-`;
+export const comedySubtitle: string = "Humorous and witty novels that entertain while offering clever social commentary.";
+
+export const yaGoldSubtitle: string = "I was a teenager once.";
+
+
 export const yaGoldArr : Book[] = [
     {title: "The Song of Achilles", author: "Madeline Miller"},
     {title: "Carry On", author: "Rainbow Rowell"},
     {title: "New Moon", author: "Stephanie Myer"},
 ]; 
-
-export const comedy: string = `
-\u2022 Jitterbug perfume, by Tom Robbins
-\u2022 Still Life With Woodpecker by Tom Robbins
-\u2022 High Maintenance by Jennifer Belle
-\u2022 Crazy Rich Asians by Kevin Kwan
-
-`;
 
 export const comedyArr : Book[] = [
     {title: "Jitterbug Perfume", author: "Tom Robbins"},
@@ -32,17 +25,6 @@ export const comedyArr : Book[] = [
     {title: "High Maintenance", author: "Jennifer Belle"},
     {title: "Crazy Rich Asians", author: "Kevin Kwan"}
 ]
-
-export const literaryFiction: string = `
-\u2022 The Secret History by Donna Tartt
-\u2022 Confessions of a Mask by Yukio Mishima
-\u2022 The Pillars of the Earth by Ken Follett
-\u2022 World Without End by Ken Follett
-\u2022 American Psycho by Bret Easton Ellis
-\u2022 The Dutch House by Ann Patchett
-\u2022 The Book of Salt by Madeline Truong
-
-`;
 
 export interface Book {
     title: string;
@@ -58,18 +40,6 @@ export const litFictionArr : Book[] = [
     {title: "The Dutch House", author: "Ann Patchett" },
     {title: "The Book of Salt", author: "Madeline Truong" }
 ]
-
-export const classics:string = `
-\u2022 Jane Eyre by Charlotte Brontë
-\u2022 Anna Karenina by Leo Tolstoy
-\u2022 Pride and Prejudice by Jane Austen
-\u2022 Ladies of the Rachmaninoff Eyes by Henry Van Dyke
-\u2022 Love in the Time of Cholera by Gabriel García Márquez
-\u2022 One Hundred Years of Solitude by Gabriel García Márquez
-\u2022 The Wrath of Dionysus by Nagrodskaia Evdokia
-\u2022 Swann's Way by Marcel Proust
-
-`;
 
 export const classicsArr : Book[] = [
     {title: "Jane Eyre", author: "Charlotte Brontë" },
--- app/components/LibraryAccordion.tsx
+++ app/components/LibraryAccordion.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Accordion, AccordionItem } from "@heroui/accordion";
 import {
   litFictionArr,
@@ -9,64 +10,37 @@ import {
 
 interface GenreProps {
   bookArr: Book[];
-  title: string;
 }
 
-function Genre({ bookArr, title }: GenreProps) {
+function Genre({ bookArr }: GenreProps) {
   return (
-    <AccordionItem key={title} title={title}>
-      <ul className="list-disc pl-4 pb-4">
-        {bookArr.map((book) => (
-          <li key={book.title}>
-            <strong>
-              <em>{book.title}</em>
-            </strong>{" "}
-            by {book.author}
-          </li>
-        ))}
-      </ul>
-    </AccordionItem>
+    <ul className="list-disc pl-4 pb-4">
+      {bookArr.map((book) => (
+        <li key={book.title}>
+          <strong>
+            <em>{book.title}</em>
+          </strong>{" "}
+          by {book.author}
+        </li>
+      ))}
+    </ul>
   );
 }
 
 export default function LibraryAccordion() {
   return (
     <Accordion>
+      <AccordionItem key="1" aria-label="Accordion 1" title="Literary Fiction">
+        <Genre bookArr={litFictionArr} />
+      </AccordionItem>
       <AccordionItem key="2" aria-label="Accordion 2" title="Classics">
-        <ul className="list-disc pl-4 pb-4">
-          {classicsArr.map((book) => (
-            <li key={book.title}>
-              <strong>
-                <em>{book.title}</em>
-              </strong>{" "}
-              by {book.author}
-            </li>
-          ))}
-        </ul>
+        <Genre bookArr={classicsArr} />
       </AccordionItem>
       <AccordionItem key="3" aria-label="Accordion 3" title="Comedy">
-        <ul className="list-disc pl-4 pb-4">
-          {comedyArr.map((book) => (
-            <li key={book.title}>
-              <strong>
-                <em>{book.title}</em>
-              </strong>{" "}
-              by {book.author}
-            </li>
-          ))}
-        </ul>
+        <Genre bookArr={comedyArr} />
       </AccordionItem>
       <AccordionItem key="4" aria-label="Accordion 4" title="YA Gold">
-        <ul className="list-disc pl-4 pb-4">
-          {yaGoldArr.map((book) => (
-            <li key={book.title}>
-              <strong>
-                <em>{book.title}</em>
-              </strong>{" "}
-              by {book.author}
-            </li>
-          ))}
-        </ul>
+        <Genre bookArr={yaGoldArr}></Genre>
       </AccordionItem>
     </Accordion>
   );
