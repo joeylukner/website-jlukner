ef62f1073ac481da259db65557d168fd34a625ec
Joey Lukner <josephlukner@icloud.com>
2026-01-21 22:00:46 -0600

    Playing with cover images

--- app/components/LibraryAccordion.tsx
+++ app/components/LibraryAccordion.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Accordion, AccordionItem } from "@heroui/accordion";
-import Image from "next/image";
+import { Image, ScrollShadow } from "@heroui/react";
 
 import {
   litFictionArr,
@@ -40,6 +40,52 @@ export default function LibraryAccordion() {
       </AccordionItem>
       <AccordionItem key="3" aria-label="Accordion 3" title="Comedy">
         <Genre bookArr={comedyArr} />
+        <ScrollShadow orientation="horizontal">
+          <div className="flex flex-row ">
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+            <Image
+              alt="JitterbugPerfume"
+              className="m-5"
+              src="/Jitterbug Perfume.jpg"
+              width={180}
+            />
+          </div>
+        </ScrollShadow>
       </AccordionItem>
       <AccordionItem key="4" aria-label="Accordion 4" title="YA Gold">
         <Genre bookArr={yaGoldArr}></Genre>
